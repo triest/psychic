e3a039ff207e7cacac50029a75d5540262db2f9b
triest <triest21@gmail.com>
2021-10-10 13:50:42 +0300

    Add files before upload

--- resources/views/gusses.blade.php
+++ resources/views/gusses.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app',['title'=>'Догадки'])
 
 @section('content')
     <div class="container">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app',['title'=>'Загадате число!'])
 
 @section('content')
 <div class="container">
